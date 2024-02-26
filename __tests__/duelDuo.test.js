a22d0b46dfa0629db6416f0a25f333b317057586
sh0stik <dariyashostak@gmail.com>
2024-02-25 17:29:16 -0800

    Comleted steps 4 and 5

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -15,4 +15,16 @@ describe("Duel Duo tests", () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+  test("Check that clicking the Draw button displays the div with id = choices", async () => {
+    await driver.get("http://localhost:8000")
+    await driver.findElement(By.id("draw")).click()
+    await driver.wait(until.elementLocated(By.id("choices")), 1000)
+  });
+  test("Check that clicking an “Add to Duo” button displays the div with id = “player-duo”", async () => {
+    await driver.get("http://localhost:8000")
+    await driver.findElement(By.id("draw")).click()
+    await driver.wait(until.elementsLocated(By.id("choices")), 1000)
+    await driver.findElement(By.className("bot-btn")).click()
+    await driver.wait(until.elementLocated(By.id("player-duo")), 1000)
+  });
 });
--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require("express");
 const bots = require("./src/botsData");
 const shuffle = require("./src/shuffle");
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'b2f02af622c841899f1a9cecc9919fc9',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const playerRecord = {
   wins: 0,
   losses: 0,
@@ -37,9 +48,10 @@ const calculateHealthAfterAttack = ({ playerDuo, compDuo }) => {
 
 app.get("/api/robots", (req, res) => {
   try {
+    rollbar.info("GET request to /api/robots")
     res.status(200).send(botsArr);
   } catch (error) {
-    console.error("ERROR GETTING BOTS", error);
+    rollbar.error("ERROR GETTING BOTS", error);
     res.sendStatus(400);
   }
 });
@@ -47,9 +59,10 @@ app.get("/api/robots", (req, res) => {
 app.get("/api/robots/shuffled", (req, res) => {
   try {
     let shuffled = shuffle(bots);
+    rollbar.info("GET request to /api/robots/shuffled")
     res.status(200).send(shuffled);
   } catch (error) {
-    console.error("ERROR GETTING SHUFFLED BOTS", error);
+    rollbar.error("ERROR GETTING SHUFFLED BOTS", error);
     res.sendStatus(400);
   }
 });
@@ -62,7 +75,7 @@ app.post("/api/duel", (req, res) => {
       compDuo,
       playerDuo,
     });
-
+    rollbar.info("POST request to /api/duel")
     // comparing the total health to determine a winner
     if (compHealth > playerHealth) {
       playerRecord.losses += 1;
@@ -72,20 +85,21 @@ app.post("/api/duel", (req, res) => {
       res.status(200).send("You won!");
     }
   } catch (error) {
-    console.log("ERROR DUELING", error);
+    rollbar.error("ERROR DUELING", error);
     res.sendStatus(400);
   }
 });
 
 app.get("/api/player", (req, res) => {
   try {
+    rollbar.info("GET request to /api/player")
     res.status(200).send(playerRecord);
   } catch (error) {
-    console.log("ERROR GETTING PLAYER STATS", error);
+    rollbar.error("ERROR GETTING PLAYER STATS", error);
     res.sendStatus(400);
   }
 });
 
 app.listen(8000, () => {
-  console.log(`Listening on 8000`);
+  rollbar.log(`Listening on 8000`);
 });
