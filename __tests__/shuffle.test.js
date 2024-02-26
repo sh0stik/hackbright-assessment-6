0c1f1a4be0f697ce60241e67cd8b56b6a21bebbb
sh0stik <dariyashostak@gmail.com>
2024-02-25 16:12:34 -0800

    Added manual testing results and unit tests

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,5 +1,23 @@
 const shuffle = require("../src/shuffle");
 
-describe("shuffle should...", () => {
-  // CODE HERE
+describe("shuffle should return an array", () => {
+  test("check that shuffle returns an array", () => {
+    expect(Array.isArray(shuffle([1, 2, 3, 4, 5]))).toBe(true);
+  });
+
+  test("check that it returns an array of the same length as the argument sent in", () => {
+    expect(shuffle([1, 2, 3, 4, 5]).length).toBe(5);
+  });
+  test("check that all the same items are in the array", () => {
+    const array = [1, 2, 3, 4, 5];
+    const shuffled = shuffle(array);
+    expect(shuffled).toEqual(expect.arrayContaining(array));
+  });
+
+  test("check that the array is shuffled", () => {
+    const array = [1, 2, 3, 4, 5];
+    const shuffled = shuffle(array);
+    expect(shuffled).not.toEqual(array);
+  });
+
 });
